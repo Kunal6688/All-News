f57c504ee31aa398823c299c97c297696fec5ac7
KUNAL JANGID <129617135+Kunal6688@users.noreply.github.com>
2024-12-01 12:41:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,50 @@
-
 const apiUrl = `https://google-news13.p.rapidapi.com/business?lr=en-US`;
 
-    async function setValue() {
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            const articles = data.data;
-            const container = $("#news-container");
+async function setValue(apiUrl) {
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '393d001e34mshd93a6eb015776e8p18dc36jsn28c0537eaca0',
+                'X-RapidAPI-Host': 'google-news13.p.rapidapi.com'
+            }
+        });
+        const data = await response.json();
+        const articles = data.data;
+        const container = $("#news-container");
 
-            // Clear previous content
-            container.empty();  
-            
-            console.log(articles);
+        // Clear previous content
+        container.empty();  
+        
+        console.log(articles);
 
-            articles.forEach(article => {
-                const cardHtml = `
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img src="${article.imageUrl}" class="card-img-top" alt="${article.title}">
-                            <div class="card-body">
-                                <h5 class="card-title">${article.title}</h5>
-                                <p class="card-text">${article.content}</p>
-                                <a href="${article.readMoreUrl}" class="btn btn-primary">Read More</a>
-                            </div>
+        articles.forEach(article => {
+            const cardHtml = `
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="${article.imageUrl}" class="card-img-top" alt="${article.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${article.title}</h5>
+                            <p class="card-text">${article.content}</p>
+                            <a href="${article.readMoreUrl}" class="btn btn-primary">Read More</a>
                         </div>
                     </div>
-                `;
-                container.append(cardHtml);
-            });
-        } catch (error) {
-            console.error("Error fetching news:", error);
-        }
+                </div>
+            `;
+            container.append(cardHtml);
+        });
+    } catch (error) {
+        console.error("Error fetching news:", error);
     }
+}
 
-    setValue();
+// Initial call
+setValue(apiUrl);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(".home").on("click",function(){
+$(".home").on("click", function() {
     console.log("hello");
-    location.reload;
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    location.reload();
+});
 
 $('.dropdown-item').on('click', function(event) {
     event.preventDefault();
@@ -76,40 +54,7 @@ $('.dropdown-item').on('click', function(event) {
 
     $(".category-name").text(selectedCategory);
     
-
     const apiUrl = `https://google-news13.p.rapidapi.com/${selectedCategory}?lr=en-US`;
 
-    async function setValue() {
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            const articles = data.data;
-            const container = $("#news-container");
-
-            // Clear previous content
-            container.empty();  
-            
-            console.log(articles);
-
-            articles.forEach(article => {
-                const cardHtml = `
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img src="${article.imageUrl}" class="card-img-top" alt="${article.title}">
-                            <div class="card-body">
-                                <h5 class="card-title">${article.title}</h5>
-                                <p class="card-text">${article.content}</p>
-                                <a href="${article.readMoreUrl}" class="btn btn-primary">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                container.append(cardHtml);
-            });
-        } catch (error) {
-            console.error("Error fetching news:", error);
-        }
-    }
-
-    setValue();
+    setValue(apiUrl);
 });
